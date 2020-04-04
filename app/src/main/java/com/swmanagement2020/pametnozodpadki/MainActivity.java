4501ed15d3dc0b61bf0946102bf4d56d60904f91
Martin Strekelj <martin.strekelj123@gmail.com>
2020-04-04 20:12:03 +0200

    Toolbar

--- app/src/main/java/com/swmanagement2020/pametnozodpadki/MainActivity.java
+++ app/src/main/java/com/swmanagement2020/pametnozodpadki/MainActivity.java
@@ -1,15 +1,32 @@
 package com.swmanagement2020.pametnozodpadki;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "Main Activity";
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+//        setActionBar(myToolbar);
+
     }
+
 
 }
