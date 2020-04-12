52ea2c5cd2b01d9338baba8fd1d479b6ce8d05b1
Martin Strekelj <martin.strekelj123@gmail.com>
2020-04-12 17:06:02 +0200

    Preference se upoštevajo na locevanje activity

--- app/src/main/java/com/swmanagement2020/pametnozodpadki/LocevanjeActivity.java
+++ app/src/main/java/com/swmanagement2020/pametnozodpadki/LocevanjeActivity.java
@@ -1,14 +1,18 @@
 package com.swmanagement2020.pametnozodpadki;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
 public class LocevanjeActivity extends AppCompatActivity {
 //    Podlaga za spodnje fragmente
@@ -23,6 +27,12 @@ public class LocevanjeActivity extends AppCompatActivity {
         if (ab != null) {
             ab.setDisplayHomeAsUpEnabled(true);
         }
+//      Vzami regijo iz Sharedpreferences na kreacijo tega acitivitya
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String regija = prefs.getString("regija", "" );
+        String izpis  = String.format("Trenunto ločuješ v %s regija", regija);
+        TextView kjeLocujem = findViewById(R.id.trenutnoLocevanje);
+        kjeLocujem.setText(izpis);
     }
 //  Fragmenti oziroma step by step funkcionalnosti -> 1. vpisi, 2. isci , itd.
     public static class Vpisi extends Fragment {
--- app/src/main/java/com/swmanagement2020/pametnozodpadki/SettingsActivity.java
+++ app/src/main/java/com/swmanagement2020/pametnozodpadki/SettingsActivity.java
@@ -1,13 +1,17 @@
 package com.swmanagement2020.pametnozodpadki;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NavUtils;
+import androidx.preference.ListPreference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 public class SettingsActivity extends AppCompatActivity {
     public static final String TAG = "Settings Activity";
@@ -26,11 +30,20 @@ public class SettingsActivity extends AppCompatActivity {
         if (ab != null) {
             ab.setDisplayHomeAsUpEnabled(true);
         }
+        //Shrani regijo v Shared preference
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = prefs.edit();
+        View izbranaRegija = findViewById(R.id.regija);
+        if (izbranaRegija != null){
+            editor.putString("regija", izbranaRegija.toString() );
+            editor.apply();
+        }
     }
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.preferences, rootKey);
         }
+
     }
 }
